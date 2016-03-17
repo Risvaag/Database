eccaa21ddbbb8377afb89023396bc0a78a60c507
Risvaag <andrris@stud.ntnu.no>
2016-03-17 12:01:47 +0100

    added querries

---
+++ Database/src/main/Querries.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Querries {
+	
+	public ResultSet getOving(String type, Connection con){
+		ResultSet ovelse;
+		String sqlEnum = "";
+		boolean first = true;
+		
+		try{
+			Statement s = con.createStatement();
+			ResultSet result = s.executeQuery("SELECT ovelse_id FROM " + "ovelsestype WHERE type="+type);
+			if(result.next()){
+				sqlEnum = "id=" + result.getString("ovelse_id");
+			}
+			while(result.next()){
+				sqlEnum += "OR id= "+result.getString("ovelse_id");
+			}
+			ovelse = s.executeQuery("SELECT * FROM Ovelse WHERE "+ sqlEnum);
+		}catch(SQLException e){
+//			System.err.println(e);
+			e.printStackTrace();
+			return null;
+		}
+		
+		return ovelse;
+	}
+
+}
