5c93960d83b0cd05a10a5bd324c8a02dede0b7a4
Risvaag <andrris@stud.ntnu.no>
2016-03-17 19:24:04 +0100

    tons of changes

--- Database/src/main/Main.java
+++ Database/src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
 public class Main {
@@ -19,13 +28,28 @@ public class Main {
 			e.printStackTrace();
 		}
   }
+  
+  
+  private static Date handleTimestamp(String dateString){
+	 SimpleDateFormat format = new SimpleDateFormat("dd:MM:yyyy");
+	 try {
+		java.util.Date parsed = format.parse(dateString);
+		return new Date(parsed.getTime());
+	} catch (ParseException e) {
+		e.printStackTrace();
+	}
+	 System.out.println("null value!");
+	 return null;
+  }
+  
 
   private static void printExercises(ResultSet res){
 	  try {
 		  int i = 0;
+		  System.out.println("ID\t:\tName");
 		while(res.next()){
 			i++;
-			  System.out.println(res.getString("id")+" : " + res.getString("navn"));
+			  System.out.println(res.getString("id")+"\t:\t" + res.getString("navn"));
 		  }
 	} catch (SQLException e) {
 		e.printStackTrace();
@@ -33,9 +57,11 @@ public class Main {
   }
 
   private static int selectExercise(Scanner scanner){
-	  System.out.println("Select the ID of the exercise you wish to add: ");
+	  System.out.println("\nSelect the ID of the exercise you wish to add: ");
 	  try{
-		  return scanner.nextInt();
+		  int value = scanner.nextInt();
+		  scanner.nextLine();
+		  return value;
 	  }catch(Exception e){
 		  e.printStackTrace();
 	  }
@@ -51,16 +77,21 @@ public class Main {
 			exercises.add(id);
 		}
   }
+  
+  public static void addResult(Connection con, Scanner scanner){
+	  
+  }
 
   public static void startApp(Connection con) {
 	Scanner scanner = new Scanner(System.in);
 
-    String help = "\n1. add\t\t- add new workout\n"+
-	          "2. help\t\t- show all commands\n" +
-	          "3. search\t- find exercises\n" +
-	          "4. exit\t\t- quit this program\n"+
-            "5. log \t\t- write a new log entry\n"+
-            "6. show log \t- Show the log";
+    String help = "\n1. add workout\t- add new workout\n"+
+    		"2. add exercise\t- add new workout\n"+
+	          "3. help\t\t- show all commands\n" +
+	          "4. search\t- find exercises\n" +
+	          "5. exit\t\t- quit this program\n"+
+            "6. log \t\t- write a new log entry\n"+
+            "7. show log \t- Show the log";
     System.out.println(help);
 
     boolean running = true;
@@ -73,8 +104,13 @@ public class Main {
 	    	  System.out.println("See you!");
 	    	  running = false;
 	          break;
-	      case "add":
+	      case "add exercise":
 	    	// Start add new workout
+	    	  System.out.println("Enter the ID of the workout you want to edit");
+	    	  addExercise(con, scanner,scanner.nextInt());
+	    	  break;
+	      case "add workout":
+	    	  //TODO add workouts
 	    	  addWorkout(con, scanner);
 	    	  break;
 	      case "help":
@@ -101,15 +137,50 @@ public class Main {
       }
     }
   }
-  private static void newLogEntry(){
+  private static void newLogEntry(String string){
     
   }
   private static String getLogEntries(){
     String entries = "A lot of entries";
     return entries;
   }
-  private static void addWorkout(Connection con, Scanner scanner){
-
+  
+  
+  private static void addWorkout(Connection con, Scanner scanner)
+  {
+	  String note = "'No note added'";
+	  System.out.println("What is the desired duration of the workout? ");
+	  System.out.println(">> ");
+	  float duration = scanner.nextFloat();
+	  scanner.nextLine();
+	  System.out.println("What date is the workout on? [dd:MM:yyyy] \n>> ");
+	  String day = scanner.nextLine();
+	  //System.out.println("What time is the workout on? [hh:mm] \n>> ");
+	  //String time = scanner.nextLine();
+	  Date date = handleTimestamp(day);//+" "+time+":"+"00.000");
+	  System.out.println("add a note? [y/n] ");
+	  String response = scanner.nextLine().toLowerCase();
+	  if(response.equals("y") || response.equals("yes")){
+		  System.out.println("Enter note (only one line): ");
+		  System.out.println(">> ");
+		  note = "'"+scanner.nextLine()+"'";
+	  }
+	  System.out.println(date);
+	  try{
+		  Statement s = con.createStatement();
+		  System.out.println();
+		  s.executeUpdate("INSERT INTO Trening (dato, varighet, notat) VALUES ('"+date+"',"+duration+","+ note +")");
+	  }catch(SQLException e){
+		  e.printStackTrace();
+	  }
+	//new Date()  
+  }
+  
+  
+  private static void addExercise(Connection con, Scanner scanner, int workout){
+	  scanner.nextLine();
+	  //TODO get next training ID
+	  
 	  List<Integer> exercises = new ArrayList<>();
 	  while (true) {
 		  //TODO make this not shitty
@@ -147,6 +218,13 @@ public class Main {
 		  }
 		  else if(response.equals("n") || response.equals("no")){
 			  for(int i = 0; i < exercises.size(); i++){
+				  try{
+					  Statement s = con.createStatement();
+					  s.executeUpdate("INSERT INTO TreningsOvelse (trening_id, ovelse_id) VALUES("+ workout+", "+ exercises.get(i) +")");
+				  }catch(SQLException e)
+				  {
+					  e.printStackTrace();
+				  }
 				  System.out.println(exercises.get(i));
 			  }
 			  break;
@@ -160,12 +238,15 @@ public class Main {
 
 	  System.out.println("Total reps: ");
 	  int reps = scanner.nextInt();
+	  scanner.nextLine();
 
 	  System.out.println("Total sets: ");
 	  int sets = scanner.nextInt();
+	  scanner.nextLine();
 
 	  System.out.println("Total weight: ");
 	  int weight = scanner.nextInt();
+	  scanner.nextLine();
 
   }
 
@@ -175,12 +256,15 @@ public class Main {
 
 	  System.out.println("Total reps: ");
 	  int reps = scanner.nextInt();
+	  scanner.nextLine();
 
 	  System.out.println("Total sets: ");
 	  int sets = scanner.nextInt();
+	  scanner.nextLine();
 
 	  System.out.println("Total weight: ");
 	  int weight = scanner.nextInt();
+	  scanner.nextLine();
   }
 
   private static void EnduranceExercise(Connection con, Scanner scanner) {
@@ -189,9 +273,11 @@ public class Main {
 
 	  System.out.println("Total length: ");
 	  int length = scanner.nextInt();
+	  scanner.nextLine();
 
 	  System.out.println("Total time: ");
 	  int time = scanner.nextInt();
+	  scanner.nextLine();
   }
 
   private static void search(Connection con, String seek, Scanner scanner){
