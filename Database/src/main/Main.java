cd9722961336396d616f11f6c168547f2b985c8c
Espen Sivertsen <sivertsen.espen@hotmail.com>
2016-03-17 10:36:39 +0100

    StartApp
    Created start menu

--- Database/src/main/Main.java
+++ Database/src/main/Main.java
@@ -11,42 +11,62 @@ import java.util.Scanner;
 public class Main {
   public static void main(String args[]){
 
-    try{
+    try	{
 			Connection con = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/andrris_trening","andrris_db1", "cdji2005");
 			System.out.println("connection established");
-			// return con;
+			startApp(con);
 		}
 		catch(SQLException e)
 		{
 			e.printStackTrace();
-			// return null;
 		}
+  }
+    
+  public static void startApp(Connection con) {
+	Scanner scanner = new Scanner(System.in);
+    
+    String help = "add \t- add new workout\n"+
+	          "show \t- show workouts/exercises\n" +
+	          "search \t- find exercise\n" +
+	          "exit \t- to quit this program\n";
+    print(help);
 
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Ready\n>> ");
-    while (scanner.hasNext()){
+    boolean running = true;
+    
+    while (running){
       String input = scanner.nextLine();
-      if (input.equals("exit")){
-        print("See you!");
-        break;
+      switch (input) {
+	      case "exit":
+	    	  print("See you!");
+	    	  running = false;
+	          break;
+	      case "add":
+	    	// Start add new workout
+	    	  addWorkout(con, scanner);
+	    	  break;
+	      case "help":
+	    	//list all commands
+	          print(help);
+	          break;
+	      case "search":
+	    	  print("Enter your search for traning");
+//	    	  search()
+	    	  break;
+	      default:
+	    	  print("Not a valid command, type help to see commands");
+	    	  break;
       }
-      if (input.equals("help")){
-        //list all commands
-        String help = "add - add new workout\n"+
-        "show - show workouts/exercises\n" +
-        "search - find exercise\n" +
-        "exit - to quit this program";
-        print(help);
-      }
-
-
-
-      System.out.print(">> ");
     }
   }
-  private static void addWorkout(){
-
+  private static void addWorkout(Connection con, Scanner scanner){
+	  
+	  boolean addExercise = true;
+	  while (addExercise) {
+		  print("Which type of exercise would you add?\n" +
+				  "type strength or cardio");
+		  
+	  }
+	  
   }
   private static void showWorkout(){
 
